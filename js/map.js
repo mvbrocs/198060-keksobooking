824384201d1ec4a07d9f2e50f9d1c768ebd09751
Maksim <m.belov@very-good.ru>
2018-06-15 20:00:21 +0300

    work with card

--- js/map.js
+++ js/map.js
@@ -30,6 +30,8 @@ var FEATURES = [
   'elevator',
   'conditioner'
 ];
+var PIN_HEIGHT = 70;
+var PIN_WIDTH = 50;
 
 var getRandomInt = function (min, max) {
   var res = Math.floor(Math.random() * (max - min)) + min;
@@ -37,7 +39,7 @@ var getRandomInt = function (min, max) {
 };
 
 
-// 1. Create data
+// 1. Create advertisements data
 var createAds = function () {
   var ads = [];
 
@@ -81,15 +83,77 @@ var createAds = function () {
 var advertisements = createAds();
 
 
-// 2.
+// 2. Show map
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
 
-// 3. Create templates
+// 3. Create pins templates
 var createMarkersTemplates = function () {
+  var fragment = document.createDocumentFragment();
   var template = document.querySelector('template');
   var pin = template.content.querySelector('.map__pin');
+
+  for (var i = 0; i < ADS_NUM; i++) {
+    var clonePin = pin.cloneNode(true);
+    var cloneAvatar = clonePin.querySelector('img');
+    var ad = advertisements[i];
+
+    clonePin.style.left = (ad.location.x - (PIN_WIDTH / 2)) + 'px';
+    clonePin.style.top = (ad.location.y - PIN_HEIGHT) + 'px';
+    cloneAvatar.src = ad.author.avatar;
+
+    fragment.appendChild(clonePin);
+  }
+
+  return fragment;
 };
 
-createMarkersTemplates();
+var markersTemplates = createMarkersTemplates();
+
+
+// 4. Render pins
+var renderMarkers = function () {
+  var mapPins = document.querySelector('.map__pins');
+  mapPins.appendChild(markersTemplates);
+};
+
+renderMarkers();
+
+
+// 5. Create card template
+var createCardTemplate = function () {
+  var template = document.querySelector('template');
+  var card = template.content.querySelector('.map__card');
+  var cloneCard = card.cloneNode(true);
+  var ad = advertisements[0];
+
+  var title = cloneCard.querySelector('.popup__title');
+  title.textContent = ad.offer.title;
+
+  var address = cloneCard.querySelector('.popup__text--address');
+  address.textContent = ad.offer.address;
+
+  var price = cloneCard.querySelector('.popup__text--price');
+  price.textContent = ad.offer.price + '&#x20bd;/ночь';
+
+  var type = cloneCard.querySelector('.popup__type');
+  var adType = ad.offer.type;
+
+  if (adType === 'flat') {
+    adType = 'Квартира';
+  } else if (adType === 'house') {
+    adType = 'Дом';
+  } else if (adType === 'bungalo') {
+    adType = 'Бунгало';
+  }
+
+  type.textContent = adType;
+
+  var capacity = cloneCard.querySelector('.popup__text--capacity');
+  capacity.textContent = ad.offer.rooms + ' для ' + ad.offer.guests + ' гостей';
+
+  // Заезд после {{offer.checkin}}, выезд до {{offer.checkout}}
+  var time = cloneCard.querySelector('.popup__text--time');
+  // time.textContent = 'Заезд после'
+};
