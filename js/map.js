dac38b62ae99a24c9998e6dff849526e83ccef30
Maksim Belov <mvbrocs@gmail.com>
2018-06-16 12:10:25 +0300

    complete module3-task1

--- js/map.js
+++ js/map.js
@@ -12,23 +12,13 @@ var OFFER_TITLES = [
   'Уютное бунгало далеко от моря',
   'Неуютное бунгало по колено в воде'
 ];
-var OFFER_TYPES = [
-  'flat',
-  'house',
-  'bungalo'
-];
-var OFFER_CHECK_TIMES = [
-  '12:00',
-  '13:00',
-  '14:00'
-];
-var FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner'
+var OFFER_TYPES = ['flat', 'house', 'bungalo'];
+var OFFER_CHECK_TIMES = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 var PIN_HEIGHT = 70;
 var PIN_WIDTH = 50;
@@ -38,8 +28,6 @@ var getRandomInt = function (min, max) {
   return res;
 };
 
-
-// 1. Create advertisements data
 var createAds = function () {
   var ads = [];
 
@@ -58,9 +46,13 @@ var createAds = function () {
     ad.author = {};
     ad.author.avatar = AVATAR_PATH + avatarNum + '.png';
 
+    ad.location = {};
+    ad.location.x = getRandomInt(300, 901);
+    ad.location.y = getRandomInt(100, 501);
+
     ad.offer = {};
     ad.offer.title = OFFER_TITLES[i];
-    ad.offer.address = location.x + ', ' + location.y;
+    ad.offer.address = ad.location.x + ', ' + ad.location.y;
     ad.offer.price = getRandomInt(1000, 1000000);
     ad.offer.type = OFFER_TYPES[getRandomInt(0, OFFER_TYPES.length)];
     ad.offer.rooms = getRandomInt(1, 6);
@@ -69,11 +61,9 @@ var createAds = function () {
     ad.offer.checkout = OFFER_CHECK_TIMES[getRandomInt(0, OFFER_CHECK_TIMES.length)];
     ad.offer.features = features;
     ad.offer.description = '';
-    ad.offer.photos = [];
+    ad.offer.photos = PHOTOS.slice();
 
-    ad.location = {};
-    ad.location.x = getRandomInt(300, 901);
-    ad.location.y = getRandomInt(100, 501);
+
     ads.push(ad);
   }
 
@@ -82,13 +72,9 @@ var createAds = function () {
 
 var advertisements = createAds();
 
-
-// 2. Show map
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
-
-// 3. Create pins templates
 var createMarkersTemplates = function () {
   var fragment = document.createDocumentFragment();
   var template = document.querySelector('template');
@@ -111,8 +97,6 @@ var createMarkersTemplates = function () {
 
 var markersTemplates = createMarkersTemplates();
 
-
-// 4. Render pins
 var renderMarkers = function () {
   var mapPins = document.querySelector('.map__pins');
   mapPins.appendChild(markersTemplates);
@@ -120,8 +104,6 @@ var renderMarkers = function () {
 
 renderMarkers();
 
-
-// 5. Create card template
 var createCardTemplate = function () {
   var template = document.querySelector('template');
   var card = template.content.querySelector('.map__card');
@@ -135,7 +117,8 @@ var createCardTemplate = function () {
   address.textContent = ad.offer.address;
 
   var price = cloneCard.querySelector('.popup__text--price');
-  price.textContent = ad.offer.price + '&#x20bd;/ночь';
+  price.innerHTML = ad.offer.price + ' &#x20bd;/ночь';
+
 
   var type = cloneCard.querySelector('.popup__type');
   var adType = ad.offer.type;
@@ -147,13 +130,53 @@ var createCardTemplate = function () {
   } else if (adType === 'bungalo') {
     adType = 'Бунгало';
   }
-
   type.textContent = adType;
 
   var capacity = cloneCard.querySelector('.popup__text--capacity');
   capacity.textContent = ad.offer.rooms + ' для ' + ad.offer.guests + ' гостей';
 
-  // Заезд после {{offer.checkin}}, выезд до {{offer.checkout}}
   var time = cloneCard.querySelector('.popup__text--time');
-  // time.textContent = 'Заезд после'
+  time.textContent = 'Заезд после ' + ad.offer.checkin + ' выезд до ' + ad.offer.checkout;
+
+  var cloneFeaturesList = cloneCard.querySelector('.popup__features');
+  var cloneFeatures = cloneFeaturesList.children;
+  var templateCloneFeature = cloneFeatures[0];
+
+  cloneFeaturesList.innerHTML = '';
+
+  var adFeatures = ad.offer.features;
+
+  for (var i = 0; i < adFeatures.length; i++) {
+    var adFeature = adFeatures[i];
+    var liClone = templateCloneFeature.cloneNode(true);
+    liClone.class = '';
+    liClone.classList.add('popup__feature', ('popup__feature--' + adFeature));
+    cloneFeaturesList.appendChild(liClone);
+  }
+
+  var description = cloneCard.querySelector('.popup__description');
+  description.textContent = ad.offer.description;
+
+  var avatar = cloneCard.querySelector('.popup__avatar');
+  avatar.src = ad.author.avatar;
+
+  var photos = cloneCard.querySelector('.popup__photos');
+  var photo = cloneCard.querySelector('.popup__photo');
+  photo.src = ad.offer.photos[0];
+
+  for (var j = 1; j < PHOTOS.length; j++) {
+    var photoClone = photo.cloneNode(true);
+    photoClone.src = ad.offer.photos[j];
+    photos.appendChild(photoClone);
+  }
+
+  return cloneCard;
 };
+
+var cardTemplate = createCardTemplate();
+
+var renderCardTemplate = function () {
+  map.appendChild(cardTemplate);
+};
+
+renderCardTemplate();
