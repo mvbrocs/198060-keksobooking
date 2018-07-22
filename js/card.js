a07e34535fcc1279f5c85e9ae603e1462bf954d2
Maksim Belov <mvbrocs@gmail.com>
2018-07-22 14:30:36 +0300

    refactor card.js

--- js/card.js
+++ js/card.js
@@ -11,44 +11,49 @@
     render: function (advertData) {
       var card = document.querySelector('template').content.querySelector('.map__card').cloneNode(true);
 
+      var setFeatures = function () {
+        var featuresContainer = card.querySelector('.popup__features');
+        var features = featuresContainer.querySelectorAll('.popup__feature');
+        var advertFeatures = advertData.offer.features;
+
+        for (var i = features.length - 1; i >= 0; i--) {
+          var featureIsExist = false;
+
+          for (var j = 0; j < advertFeatures.length; j++) {
+
+            if (features[i].classList.contains('popup__feature--' + advertFeatures[j])) {
+              featureIsExist = true;
+            }
+          }
+
+          if (!featureIsExist) {
+            featuresContainer.removeChild(features[i]);
+          }
+        }
+      };
+
+      var setPhotos = function () {
+        var photos = card.querySelector('.popup__photos');
+        var photo = card.querySelector('.popup__photo');
+        photo.src = advertData.offer.photos[0];
+
+        for (var i = 1; i < advertData.offer.photos.length; i++) {
+          var clonePhoto = photo.cloneNode();
+          clonePhoto.src = advertData.offer.photos[i];
+          photos.appendChild(clonePhoto);
+        }
+      };
+
       card.querySelector('.popup__title').textContent = advertData.offer.title;
       card.querySelector('.popup__text--address').textContent = advertData.offer.address;
       card.querySelector('.popup__text--price').innerHTML = advertData.offer.price + ' &#x20bd;/ночь';
       card.querySelector('.popup__type').textContent = ADVERT_TYPE_RUS[advertData.offer.type];
       card.querySelector('.popup__text--capacity').textContent = advertData.offer.rooms + ' для ' + advertData.offer.guests + ' гостей';
       card.querySelector('.popup__text--time').textContent = 'Заезд после ' + advertData.offer.checkin + ' выезд до ' + advertData.offer.checkout;
-
-      var cardFeaturesContainer = card.querySelector('.popup__features');
-      var cardFeatures = cardFeaturesContainer.querySelectorAll('.popup__feature');
-      var advertFeatures = advertData.offer.features;
-
-      for (var i = cardFeatures.length - 1; i >= 0; i--) {
-        var featureIsExist = false;
-
-        for (var j = 0; j < advertFeatures.length; j++) {
-
-          if (cardFeatures[i].classList.contains('popup__feature--' + advertFeatures[j])) {
-            featureIsExist = true;
-          }
-        }
-
-        if (!featureIsExist) {
-          cardFeaturesContainer.removeChild(cardFeatures[i]);
-        }
-      }
-
       card.querySelector('.popup__description').textContent = advertData.offer.description;
       card.querySelector('.popup__avatar').src = advertData.author.avatar;
-
-      var photos = card.querySelector('.popup__photos');
-      var photo = card.querySelector('.popup__photo');
-      photo.src = advertData.offer.photos[0];
-
-      for (var k = 1; k < advertData.offer.photos.length; k++) {
-        var clonePhoto = photo.cloneNode();
-        clonePhoto.src = advertData.offer.photos[k];
-        photos.appendChild(clonePhoto);
-      }
+      setFeatures();
+      setPhotos();
 
       return card;
     }
