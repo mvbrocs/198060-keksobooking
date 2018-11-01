6904baa054562e2b75d724fa1d43ddb5e580467c
Maksim <m.belov@very-good.ru>
2018-11-01 18:55:05 +0300

    refactor pins

--- js/pins.js
+++ js/pins.js
@@ -1,62 +1,47 @@
 'use strict';
 
 (function () {
-  var PIN_MAIN_SPIKE_HEIGHT = 16;
-  var PIN_MAIN_WIDTH = 65;
-  var PIN_MAIN_HEIGHT = 65 + PIN_MAIN_SPIKE_HEIGHT;
-  var PIN_HEIGHT = 70;
-  var PIN_WIDTH = 50;
+  var pinsContainer = document.querySelector('.map__pins');
+  var pinTemplate = window.util.getTemplate('.map__pin');
 
-  var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
-  var createMapPin = function (advertData) {
+  var Pin = {
+    WIDTH: 50,
+    HEIGHT: 70
+  };
+
+  var _createPin = function (advertData) {
     var pin = pinTemplate.cloneNode(true);
 
-    pin.style.left = (advertData.location.x - (PIN_WIDTH / 2)) + 'px';
-    pin.style.top = (advertData.location.y - PIN_HEIGHT) + 'px';
+    pin.style.left = (advertData.location.x - Pin.WIDTH / 2) + 'px';
+    pin.style.top = (advertData.location.y - Pin.HEIGHT) + 'px';
 
-    pin.querySelector('img').src = advertData.author.avatar;
+    pin.querySelector('img').src = advertData['author']['avatar'];
 
     return pin;
   };
 
-  var activatePin = function (pin) {
-    pin.classList.add('map__pin--active');
-  };
-
-  var pinHandler = function (pin, advertData) {
-
-    pin.addEventListener('click', function () {
-      activatePin(pin);
-      window.map.openPopup(advertData);
-    });
-  };
-
-  var pinsContainer = document.querySelector('.map__pins');
-
   window.pins = {
-    pinMain: {
-      node: document.querySelector('.map__pin--main'),
-      width: PIN_MAIN_WIDTH,
-      height: PIN_MAIN_HEIGHT
-    },
-    render: function (advertsData) {
+    render: function (data) {
 
-      for (var i = 0; i < advertsData.length; i++) {
-        var pin = createMapPin(advertsData[i]);
-        pinHandler(pin, advertsData[i]);
+      data.forEach(function (advertData) {
+        var pin = _createPin(advertData);
+
+        pin.addEventListener('click', function () {
+          pin.classList.add('map__pin--active');
+          window.popup.open(advertData);
+        });
+
         pinsContainer.appendChild(pin);
-      }
+      });
     },
     deactivate: function () {
-      var pins = document.querySelectorAll('.map__pin');
+      var existingPins = pinsContainer.querySelectorAll('.map__pin');
 
-      for (var i = 0; i < pins.length; i++) {
-        var pin = pins[i];
-
+      [].forEach.call(existingPins, function (pin) {
         if (pin.classList.contains('map__pin--active')) {
           pin.classList.remove('map__pin--active');
         }
-      }
+      });
     }
   };
 })();
