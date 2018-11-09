1b00430a29f0792f7060a53d2208c706413d4d5d
Maksim <m.belov@very-good.ru>
2018-11-09 20:19:59 +0300

    work with filter

--- js/filter.js
+++ js/filter.js
@@ -6,7 +6,7 @@
   filter.type = document.querySelector('#housing-type').value;
   filter.price = document.querySelector('#housing-price').value;
   filter.rooms = document.querySelector('#housing-rooms').value;
-  filter.guests = document.querySelector('#housing-rooms').value;
+  filter.guests = document.querySelector('#housing-guests').value;
   filter.features = {};
   filter.features.wifi = document.querySelector('#filter-wifi').checked;
   filter.features.dishwasher = document.querySelector('#filter-dishwasher').checked;
@@ -15,6 +15,14 @@
   filter.features.elevator = document.querySelector('#filter-elevator').checked;
   filter.features.conditioner = document.querySelector('#filter-elevator').checked;
 
+  var filterSelects = document.querySelectorAll('.map__filter');
+
+  [].forEach.call(filterSelects, function (select) {
+
+    select.addEventListener('change', function () {
+      var filterName = select.name.split()
+    });
+  });
 
   // var filter = {
   //   'type': 'any',
