8316f7a6158cb5b171d7932f93dd304bc6224db4
Maksim <m.belov@very-good.ru>
2018-11-09 20:19:59 +0300

    work with filter 1

--- js/filter.js
+++ js/filter.js
@@ -1,20 +1,39 @@
 'use strict';
 
 (function () {
-  var filter = {};
+  var selectType = document.querySelector('#housing-type');
+  var selectPrice = document.querySelector('#housing-price');
+  var selectRooms = document.querySelector('#housing-rooms');
+  var selectGuests = document.querySelector('#housing-guests');
 
-  filter.type = document.querySelector('#housing-type').value;
-  filter.price = document.querySelector('#housing-price').value;
-  filter.rooms = document.querySelector('#housing-rooms').value;
-  filter.guests = document.querySelector('#housing-rooms').value;
-  filter.features = {};
-  filter.features.wifi = document.querySelector('#filter-wifi').checked;
-  filter.features.dishwasher = document.querySelector('#filter-dishwasher').checked;
-  filter.features.parking = document.querySelector('#filter-parking').checked;
-  filter.features.washer = document.querySelector('#filter-washer').checked;
-  filter.features.elevator = document.querySelector('#filter-elevator').checked;
-  filter.features.conditioner = document.querySelector('#filter-elevator').checked;
+  var checkWifi = document.querySelector('#filter-wifi');
+  var checkDishwasher = document.querySelector('#filter-dishwasher');
+  var checkParking = document.querySelector('#filter-parking');
+  var checkWasher = document.querySelector('#filter-washer');
 
+  var houseType;
+  // var filter = {};
+  //
+  // filter.type = document.querySelector('#housing-type').value;
+  // filter.price = document.querySelector('#housing-price').value;
+  // filter.rooms = document.querySelector('#housing-rooms').value;
+  // filter.guests = document.querySelector('#housing-guests').value;
+  // filter.features = {};
+  // filter.features.wifi = document.querySelector('#filter-wifi').checked;
+  // filter.features.dishwasher = document.querySelector('#filter-dishwasher').checked;
+  // filter.features.parking = document.querySelector('#filter-parking').checked;
+  // filter.features.washer = document.querySelector('#filter-washer').checked;
+  // filter.features.elevator = document.querySelector('#filter-elevator').checked;
+  // filter.features.conditioner = document.querySelector('#filter-elevator').checked;
+  //
+  // var filterSelects = document.querySelectorAll('.map__filter');
+  //
+  // [].forEach.call(filterSelects, function (select) {
+  //
+  //   select.addEventListener('change', function () {
+  //     var filterName = select.name.split()
+  //   });
+  // });
 
   // var filter = {
   //   'type': 'any',
