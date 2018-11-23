23aee39ac840e0f8e3230b13052cc96b45365fca
Maksim <m.belov@very-good.ru>
2018-11-23 17:36:06 +0300

    complete preview file reader

--- js/ad-photos.js
+++ js/ad-photos.js
@@ -3,9 +3,9 @@
 (function () {
   var inputFile = document.querySelector('#images');
   var photoContainer = document.querySelector('.ad-form__photo-container');
-  var photos = document.querySelectorAll('.ad-form__photo');
 
   var onLoad = function (reader) {
+    var photos = document.querySelectorAll('.ad-form__photo');
     var lastPhoto = photos[photos.length - 1];
     var clone = lastPhoto.cloneNode(true);
     var img = document.createElement('img');
